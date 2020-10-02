f1fca2522654393a5dbb210b97a91dbf884d1fd7
Elias Frantar <elias.frantar@gmail.com>
2020-10-02 12:02:13 +0200

    Backup

--- src/grip.cpp
+++ src/grip.cpp
@@ -52,9 +52,10 @@ namespace grip {
     {{0, 0, 0, 0}, {0, 0, 1, 0}, {1, 0, 0, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}, {1, 0, 1, 0}, {0, 1, 1, 0}, {1, 1, 1, 0}}  // rB
   };
 
-  // TODO
-  // In some situations we can
-  const move::mask CUTOPTIONS = move::bit(move::R1L2) | move::bit(move::R2L1) | move::bit(move::F1B2) | move::bit(move::F2B1);
+  const int SSCORE = 1; // save moves
+  const int CSCORE = 100; // cuts
+  const int risky_ax[] = {-1, -1, -1, -1}; // TODO
+
 
   int nextset[N_STATESETS][N_MOVES];
   int nextiset[N_STATESETS][N_MOVES];
@@ -96,90 +97,24 @@ namespace grip {
     return !(c1 == c2);
   }
 
-  /*
-   * - One extra DP state for every gripper state indicating a cutable scenario
-   * - 4 extra states indicating that a certain face did not move in a blocking state
-   *  - Note that we only have forced hard regrips and thus 4 states should be sufficient
-   */
-
-  /*
-   * Always make the choice that
-   * Always make the choice that does a half-turn in vertical position
-   */
-
-  /*
-   * - Tilts + regrips never cutable
-   * - We can always cut two consecutive move without any regrips
-   * - Out cut: no regrip or half turn + regrip
-   * - In cut: no regrip
-   * - Out cut: half-turn + regrip that starts in horizontal, half-turn + quarter-turn
-   * - In cut: half turn + regrip that starts in vertical
-   * - tFB F [rB] R is cutable if not [rF] in next move (potentially ignore this case if too hard)
-   */
-  
-  // dp[len + 1][state::COUNT][regrip::COUNT]
-  // pd[len + 1][state::COUNT][regrip::COUNT]
-
-  const move::mask TILTMASK = move::bit(move::TRL) | move::bit(move::TFB);
-  const move::mask HALFMASK =
-    move::bit(move::R2) | move::bit(move::L2) | move::bit(move::R2L1) |
-    move::bit(move::R2L2) | move::bit(move::R2L3) | move::bit(move::R1L2) || move::bit(move::R3L2)
-  ;
-
-  // Transition guaranteed to be possible
-  const bool cut(int m1, int par1, int blog1, int state, int m2, int par2, int blog2) {
-    // There are no cuts when tilts are involved
-    if ((move::bit(m1) & TILTMASK) || (move::bit(m2) & TILTMASK))
-      return false;
-
-    cube c;
-    set_state(c, state);
-
-    if (m1 == move::G)
-      return false; // we cannot cut out of any regrip
-    if (m2 == move::G) { // TODO:
-
+  // Not handled: half -> simple + par regrip; if par regrip has not been regripped in previous move
+  int cut(int m, int r, int mprev, int rprev) {
+    if (m >= move::TRL || mprev >= move::TRL)
+      return score[0][m][r]; // we can never cut with a tilt/regrip involved
+    if (m % 15 < 6 && (m % 15) % 3 != 1) { // simple move
+      if (r == regrip::_) // if no regrip
+        return SSCORE + CSCORE;
+      // If prev move was a half-turn with at most a parallel regrip
+      if (((mprev % 15) == 10 || (mprev % 15 % 3) == 1) && rprev <= regrip::A2)
+        return SSCORE + CSCORE;
+      return SSCORE;
     }
-    // Neither `m1` nor `m2` is a full regrip
-
-    int ax1 = m1 / 15 - 1;
-    int ax2 = m2 / 15 - 1;
-    bool half1 = HALFMASK & (m1 % 15);
-    bool half2 = HALFMASK & (m2 % 15);
-
-    if (half1) {
-      int otherax = 2 * !ax1;
-      if (c.blocked[otherax] || c.blocked[otherax + 1])
-        return false; // cannot cut when regripping into HT-face
+    // Simple prev move + par regrip -> axial
+    if (mprev % 15 < 6 && (mprev % 15) % 3 != 1 && rprev == regrip::A2 && m % 15 > 6) { // rprev is 0 if possible
+      if (m == risky_ax[2 * (mprev / 15) + (mprev % 15) / 3]) // move may be risky
+        return CSCORE;
     }
-
-    if (!half2) {
-      if (par2 != regrip::_)
-        return false; // cannot cut into a non-HT with any kind of regrip
-    } else {
-      if (!half1) {
-        int otherax = 2 * !ax2;
-
-        // Cannot cut when we have to open a gripper before the HT
-        if (c.blocked[otherax] && MOVE_CUBES[m1][regrip::_].blocked[otherax])
-          return false;
-        if (c.blocked[otherax + 1] && MOVE_CUBES[m1][regrip::_].blocked[otherax + 1])
-          return false;
-
-        // Cannot cut when current pivot was regripped in previous move
-        if (par1 == regrip::A2) {
-          bool piv1 = MOVE_CUBES[m2][par2].blocked[otherax] == 0;
-          bool piv2 = MOVE_CUBES[m2][par2].blocked[otherax = 1] == 0;
-          bool reg1 = !MOVE_CUBES[m1][regrip::_].blocked[otherax] && MOVE_CUBES[m1][par1].blocked[otherax];
-          bool reg2 = !MOVE_CUBES[m1][regrip::_].blocked[otherax + 1] && MOVE_CUBES[m1][par1].blocked[otherax + 1];
-          if (piv1 != piv2) { // no problem if we have 2 pivots
-            if (piv1 && reg1 || piv2 && reg2)
-              return false;
-          }
-        }
-      }
-    }
-
+    return SSCORE + CSCORE; // in all other legal cases we can cut
   }
 
   // Only supposed to be called with actually executable solutions (i.e. with correct stateset transitions)
@@ -190,7 +125,6 @@ namespace grip {
     int len = sol.size();
     parg.resize(len);
     blog.resize(len);
-    int n_states = 2 * state::COUNT;
 
     int dp[len + 1][state::COUNT];
     int pd[len + 1][state::COUNT];
@@ -226,7 +160,10 @@ namespace grip {
           int state1 = nextstate[state][m][r];
           if (state1 == -1)
             continue;
-          if (dp[i][state] + score[state][m][r] > dp[i + 1][state1]) {
+          if (
+            dp[i][state] + score[state][m][r] > dp[i + 1][state1] ||
+            (dp[i][state] + score[state][m][r] == dp[i + 1][state1] && r < pd1[i + 1][state1]) // prefer lower rs
+          ) {
             dp[i + 1][state1] = dp[i][state] + score[state][m][r];
             pd[i + 1][state1] = state;
             pd1[i + 1][state1] = r;
